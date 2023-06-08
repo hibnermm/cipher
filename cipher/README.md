58358663e9bf5a07026445cdd25262844f76cad4
J.J. Hibner <jjmhibner@gmail.com>
2023-06-07 23:16:39 -0700

    update css styling gradient and form and button, update scripts to include cipher functions and event listeners, and add headings and form to index

---
+++ cipher/README.md
@@ -0,0 +1,43 @@
+# _Cipher_
+
+#### By _Melissa Hibner_
+
+## Technologies Used
+
+- HTML
+- CSS
+
+## Description
+
+_This project consists of a cipher template._
+
+## Setup/Installation Requirements
+
+- Clone repository from Github to your desktop:
+  `$git clone url `
+- Open index.html in a browser.
+
+## Known Bugs
+
+- The event listeners are still being worked on.
+
+## License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Copyright (c) 2023, Melissa Hibner
---
+++ cipher/js/scripts.js
@@ -0,0 +1,72 @@
+
+/*1. prompt input from user, e.g. Sentence
+let userSentence = prompt("Do you need help creating an encoded message?  Enter your message here... ");
+
+/*2. Stores input in variable and display to user 
+alert(userSentence);
+*/
+const element = document.getElementById("mybutton");
+element.addEventListener("click", myFunction);
+
+function myFunction(){
+  document.getElementById("userSentence");
+  firstLastLetters(userSentence);
+}
+
+/*3. function1 takes ONLY first and last letter of sentence, makes capital, and new string1, e.g. IE */
+function firstLastLetters(userSentence){
+  let firstLetter = userSentence.charAt(0); 
+  let sentenceLength = userSentence.length;
+  let lastLetter = userSentence.charAt(sentenceLength-1); 
+  let firstLastLetters = firstLetter+lastLetter;
+  let upperCaseLetters = firstLastLetters.toUpperCase();
+  return upperCaseLetters;
+}
+
+let uppercaseFirstLastLetters = firstLastLetters(userSentence);   
+alert(uppercaseFirstLastLetters);
+
+/*4. function2 uses new string, reverse orders first and last letter, return result only string2, e.g. EI */
+function reverseLetters(uppercaseFirstLastLetters){
+  let reverseFLLetters = uppercaseFirstLastLetters.split("").reverse().join("");
+  return reverseFLLetters;
+}
+
+let reverseTwoLetters = reverseLetters(uppercaseFirstLastLetters);
+alert(reverseTwoLetters);
+
+/*5. function3 calls function1 and function2, concats Sentence+string2 = Sentence2 aka userSentence+LF*/
+function combined(userSentence){
+  firstLastLetters(userSentence);
+  reverseLetters(uppercaseFirstLastLetters);
+  let sentenceLetters = userSentence + reverseTwoLetters;
+  return sentenceLetters;
+}
+
+let sentenceFirstLast = combined(userSentence);
+alert(sentenceFirstLast);
+
+/*6. function4 counts number of letters in userSentence, userSentence/2 -> whole number -> numberIndex, uses numberIndex to add to start of userSentence, creates final sentence with numberIndexLetter + userSentence + LF */
+function countLetters(userSentence){
+  let halfSentenceLength = Math.floor(userSentence.length/2);
+  let indexLetter = userSentence.charAt(halfSentenceLength);
+  let initialCipher = indexLetter + sentenceFirstLast;
+  return initialCipher;
+}
+
+let indexSentenceFirstLast = countLetters(userSentence);
+alert(indexSentenceFirstLast);
+
+/*7. Reverse all letters*/
+function reverseReverse(indexSentenceFirstLast){
+  let reverseCipher = indexSentenceFirstLast.split("").reverse().join("");
+  return reverseCipher;
+}
+let finalCipher = reverseReverse(indexSentenceFirstLast);
+
+/*8. Enter final cipher in HTML*/
+function finalCipherReveal(finalCipher){
+  document.getElementById("output").innerHTML = finalCipher;
+}
+
+/*alert(`Your new encoded message is ${finalCipher}`);*/
